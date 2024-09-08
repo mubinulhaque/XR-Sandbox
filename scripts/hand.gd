8521d5f0a0d50921c22d28f12d9455e500a800bc
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2024-09-08 20:45:35 +0100

    Re-add hand scene
    Add function for left-handed mode

---
+++ scripts/hand.gd
@@ -0,0 +1,32 @@
+class_name Hand
+extends XRController3D
+
+@export var use_direct_movement := false ## Whether this hand controls turning and forwards motion, or teleportation
+
+@export var options_viewport: Viewport2Din3D
+
+@onready var _function_teleport: XRToolsFunctionTeleport = $FunctionTeleport
+@onready var _movement_direct: XRToolsMovementDirect = $MovementDirect
+@onready var _movement_turn: XRToolsMovementTurn = $MovementTurn
+
+
+func _ready() -> void:
+	toggle_locomotion(use_direct_movement)
+
+
+func _on_button_pressed(button_name: String) -> void:
+	match button_name:
+		"menu_button":
+			if options_viewport:
+				options_viewport.visible = !options_viewport.visible
+				options_viewport.enabled = !options_viewport.enabled 
+
+
+func toggle_locomotion(direct_movement: bool) -> void:
+	# Toggle the locomotion options
+	_function_teleport.enabled = not direct_movement
+	_movement_direct.enabled = direct_movement
+	_movement_turn.enabled = direct_movement
+	
+	# Toggle the teleport node's visibility
+	_function_teleport.visible = not direct_movement
--- scripts/left_hand.gd
+++
@@ -1,9 +0,0 @@
-extends XRController3D
-
-@onready var _options_viewport: Viewport2Din3D = $OptionsViewport
-
-
-func _on_button_pressed(name: String) -> void:
-	if name == "menu_button":
-		_options_viewport.visible = !_options_viewport.visible
-		_options_viewport.enabled = !_options_viewport.enabled

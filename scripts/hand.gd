9d1850d314f19cdf937cc6c8654a5c1757db63c6
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2024-09-14 18:50:30 +0100

    Clean up hands
    Disable pointer unless options menu is active
    Rename left hand rig
    Make XR functions more readable

--- scripts/hand.gd
+++ scripts/hand.gd
@@ -1,10 +1,13 @@
 class_name HandController
 extends XRController3D
 
+signal options_toggled(enabled: bool)
+
 @export var use_direct_movement := false ## Whether this hand controls turning and forwards motion, or teleportation
 @export var options_viewport: Viewport2Din3D
+@export var pointer: XRToolsFunctionPointer
 
-@onready var _function_teleport: XRToolsFunctionTeleport = $FunctionTeleport
+@onready var _function_teleport: XRToolsFunctionTeleport = $Teleporter
 @onready var _movement_direct: XRToolsMovementDirect = $MovementDirect
 @onready var _movement_turn: XRToolsMovementTurn = $MovementTurn
 
@@ -19,6 +22,8 @@ func _on_button_pressed(button_name: String) -> void:
 			if options_viewport:
 				options_viewport.visible = !options_viewport.visible
 				options_viewport.enabled = !options_viewport.enabled
+				
+				options_toggled.emit(options_viewport.enabled)
 
 
 func toggle_locomotion(direct_movement: bool) -> void:
@@ -29,3 +34,10 @@ func toggle_locomotion(direct_movement: bool) -> void:
 	
 	# Toggle the teleport node's visibility
 	_function_teleport.visible = not direct_movement
+
+
+func _toggle_pointer(enabled: bool) -> void:
+	# Toggle the pointer
+	if pointer:
+		pointer.visible = !pointer.visible
+		pointer.enabled = !pointer.enabled
